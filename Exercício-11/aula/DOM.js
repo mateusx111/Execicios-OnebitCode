0f7f72e5a91d9e18c50f960bd41d26c02d0a4681
Mateus <msouza954@gmail.com>
2023-01-14 10:42:21 -0300

    exercício-11

--- Exercício-11/aula/DOM.js
+++ Exercício-11/aula/DOM.js
@@ -1,3 +1,4 @@
+//Obtendo Elementos HTML do DOM
 function show() {
   const contactList = document.getElementById('contact-list')
   console.log(contactList)
@@ -10,9 +11,81 @@ function show() {
 
   const contact1 = document.getElementsByName('contact1')
   console.log(contact1)
-  
+
   const contacts = document.querySelectorAll('#contact-list > li > label')
   console.log(contacts)
 
   const firstContact = document.querySelector('#contact-list > li > label')
+}
+
+//Criando Novos Elementos no DOM
+
+function addInput() {
+  const ul = document.getElementById('inputs')
+
+  const newLi = document.createElement('li')
+  newLi.className = 'list-item'
+  newLi.innerText = 'Novo input: '
+
+  const newInput = document.createElement('input')
+  newInput.type = 'text'
+  newInput.name = 'input'
+
+  newLi.appendChild(newInput)
+  ul.appendChild(newLi)
+}
+
+//Praticando a manipulação do DOM
+
+function addContact() {
+  const contactSection = document.getElementById('contact-list1')
+
+  const h3 = document.createElement('h3')
+  h3.innerText = "Contato"
+
+  const ul = document.createElement('ul')
+
+  const nameLi = document.createElement('li')
+  nameLi.innerText = "Nome: "
+  const nameInput = document.createElement('input')
+  nameInput.type = 'text'
+  nameInput.name = 'fullname'
+  nameLi.appendChild(nameInput)
+  ul.appendChild(nameLi)
+  ul.appendChild(document.createElement('br'))
+
+  const phoneLi = document.createElement('li')
+  phoneLi.innerText = "Telefone: "
+  const phoneInput = document.createElement('input')
+  phoneInput.type = 'text'
+  phoneInput.name = 'phone'
+  phoneLi.appendChild(phoneInput)
+  ul.appendChild(phoneLi)
+  ul.appendChild(document.createElement('br'))
+
+
+  const addressLi = document.createElement('li')
+  addressLi.innerHTML = '<label for="address">Endereço: </label>'
+  const addressInput = document.createElement('input')
+  addressInput.type = 'text'
+  addressInput.name = 'address'
+  addressInput.id = 'adress'
+  addressLi.appendChild(addressInput)
+  ul.appendChild(addressLi)
+  ul.appendChild(document.createElement('br'))
+
+
+  contactSection.append(h3, ul)
+
+}
+
+function removeContact() {
+  const contactSection = document.getElementById('contact-list1')
+  
+  const titles = document.getElementsByTagName('h3')
+  const contacts = document.getElementsByTagName('ul')
+
+  contactSection.removeChild(titles[titles.length - 1])
+  contactSection.removeChild(contacts[contacts.length - 1])
+
 }

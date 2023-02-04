e5822754e79630109e57be89a3e5ae45d3c06061
Mateus <msouza954@gmail.com>
2023-02-04 16:40:38 -0300

    revisao

--- Exercício-09/9.1/script.js
+++ Exercício-09/9.1/script.js
@@ -1,11 +1,48 @@
 function areTriangulo(base, altura) {
   base = prompt("Digite o valor da base")
   altura = prompt("Digite o valor da altura")
-  return (base * altura) / 2
+  return alert(`
+  Base = ${base} 
+  Altura = ${altura} 
+  Área = ${(base * altura) / 2}`)
 }
 
 function areRetangulo(base, altura) {
-  return (base * altura)
+  base = prompt("Digite o valor da base")
+  altura = prompt("Digite o valor da altura")
+  return alert(`
+  Base = ${base} 
+  Altura = ${altura} 
+  Área = ${base * altura}`)
+}
+
+function areaQuadrado(lado) {
+  lado = prompt("Digite o valor do lado do quadrado")
+  return alert(`
+    lado = ${lado}
+    Área = ${lado * lado}
+  `)
+}
+
+function areaTrapesio(baseM, baseN, altura) {
+  baseM= prompt("Digite o valor da base")
+  baseN = prompt("Digite o valor da base")
+  altura = prompt("Digite o valor da altura")
+  return alert(`
+    Base maior = ${baseM}
+    Base menor = ${baseN}
+    Base altura = ${altura}
+    Área = ${(baseM + baseN) * altura/2}
+  `)
+}
+
+function areaCirculo(raio, pi = 3.14) {
+  raio = prompt("Digite o valor do raio")
+  return alert(`
+    pi = 3.14
+    raio = ${raio}
+    área = ${pi * raio**2}
+  `)
 }
 
 function exibirMenu() {
@@ -27,22 +64,25 @@ function exercutar() {
     menu = exibirMenu()
     switch (menu) {
       case "1":
-        prompt(areTriangulo())
+        areTriangulo()
         break;
       case "2":
+        areRetangulo()
         break;
       case "3":
+        areaQuadrado()
         break;
       case "4":
+        areaTrapesio()
         break;
       case "5":
+        areaCirculo()
         break;
       case "6":
+        alert("Finalizando...")
         break;
-
       default:
         alert("Opção inválida")
-        break;
     }
 
   } while (menu !== "6");

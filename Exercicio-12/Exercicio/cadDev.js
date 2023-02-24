4a3876d763386231a104895f8de406824944cc80
Mateus <msouza954@gmail.com>
2023-02-24 11:33:23 -0300

    execicio

--- Exercicio-12/Exercicio/cadDev.js
+++ Exercicio-12/Exercicio/cadDev.js
@@ -8,7 +8,7 @@ function createLabel(text, htmlFor) {
 function createInput(id, value, name, type = 'text', placeholder = '') {
   const input = document.createElement('input')
   input.id = id
-  input.value = value
+  input.value = value 
   input.name = name
   input.type = type
   input.placeholder = placeholder
@@ -22,7 +22,7 @@ let inputRows = 0
 
 addTechBtn.addEventListener('click', function (ev) {
   const stackInputs = document.getElementById('stackInputs')
-  
+
   const newRow = document.createElement('li')
   const rowIndex = inputRows
   inputRows++
@@ -32,9 +32,20 @@ addTechBtn.addEventListener('click', function (ev) {
   const techNameLabel = createLabel('Nome: ', 'techName-' + rowIndex)
   const techNameInput = createInput('techName-' + rowIndex, null, 'techName')
 
+  const explabel = createLabel('Experiencia: ')
+  const id1 = 'expRadio-' + rowIndex + '.1'
+  const expRadio1 = createInput(id1 , '0-2 anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel1 = createLabel('0-2 anos', id1)
+  const id2 = 'expRadio-' + rowIndex + '.2'
+  const expRadio2 = createInput(id2 , '3-4 anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel2 = createLabel('3-4 anos', id2)
+  const id3 = 'expRadio-' + rowIndex + '.3'
+  const expRadio3= createInput(id3 , '5+ anos', 'techExp-' + rowIndex, 'radio')
+  const expLabel3 = createLabel('5+ anos', id3)
+
   newRow.append(
-    techNameLabel, techNameInput
-    )
+    techNameLabel, techNameInput, explabel, expRadio1, expLabel1, expRadio2, expLabel2, expRadio3, expLabel3
+  )
 
   stackInputs.appendChild(newRow)
 })

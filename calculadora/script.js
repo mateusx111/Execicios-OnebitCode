e34c15b13c6aab22ec97f7f77e62d6d05dcd58e9
Mateus <msouza954@gmail.com>
2023-03-01 09:11:17 -0300

    execicio

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,4 +1,4 @@
-const main = document.querySelector("#main");
+const main = document.querySelector("main");
 const root = document.querySelector(":root");
 const input = document.getElementById("input");
 const resultInput = document.getElementById("result");
@@ -27,7 +27,7 @@ const allowedKeys = [
 
 document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
   charKeyBtn.addEventListener("click", () => {
-    const value = charKeyBtn.dataset.value
+    const value = charKeyBtn.dataset.value 
     input.value += value
   });
 });
@@ -52,6 +52,39 @@ input.addEventListener("keydown", (e) => {
 document.getElementById('equal').addEventListener("click", calculate)
 
 function calculate() {
+  resultInput.value = 'ERROR'
+  resultInput.classList.add('error')
+
   const result =  eval(input.value)
+  
   resultInput.value = result
+  resultInput.classList.remove('error')
 }
+
+document.getElementById('copyToClipboard').addEventListener('click', (e) =>{
+  const button = e.currentTarget
+    if(button.innerText === 'Copy') {
+      button.innerText = 'Copied!'
+      button.classList.add('success')
+      navigator.clipboard.writeText(resultInput.value)
+    } else {
+      button.innerText = 'Copy'
+      button.classList.remove('success')
+    }
+})
+
+document.getElementById("themeSwitcher").addEventListener("click", () => {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+})
